5958e8ac68f9772b25b77c6a31844dc8857336ef
indra <prince.poudel1998@gmail.com>
2019-11-27 10:15:10 +0545

    js 1 assignment added

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 var imageCount= document.getElementById("carousel-image-wrapper").childElementCount;
 var carouselImageWrapper=document.getElementById("carousel-image-wrapper");
-carouselImageWrapper.style.width=( (imageCount*400) + imageCount*3+1)+"px";
+carouselImageWrapper.style.width=( (imageCount*400) + imageCount*3)+"px";
 carouselImageWrapper.style.height=200+"px";
+for(var i=0;i<=imageCount;i++)
+{
+var span = document.createElement("span");
+   span.style.cursor= "pointer";
+    span.style.height= "15px";
+    span.style.width=" 15px";
+    span.style.margin= "0 2px";
+    span.style["background-color"]=" #bbb";
+    span.style["border-radius"]=" 50%";
+    span.style.display=" inline-block";
+    span.style.transition=" background-color 0.6s ease";
+//document.getElementById("dot-wrapper").appendChild(span);
+}
 
 function plusSlides(n) {
     var styleCarouselImageWrapper= getComputedStyle(carouselImageWrapper);
     var posXpx= styleCarouselImageWrapper.getPropertyValue('left');
-    console.log(posXpx);
     var posX = posXpx.slice(0, -2);
-    console.log(posX);
-
     if(n===-1)
     {
        if(posX<=-(imageCount-1)*404)
@@ -19,8 +29,6 @@ function plusSlides(n) {
        }
        else{
         carouselImageWrapper.style.left = (parseInt(posX)-404)+"px";
-        console.log("pre");
-        console.log(posX);
        }
 
        
@@ -34,9 +42,14 @@ function plusSlides(n) {
        }
        else{
         carouselImageWrapper.style.left = (parseInt(posX)+404)+"px";
-        console.log("next");
-        console.log(posX);
        }
     
     }
   }
+
+  function repeat()
+  {
+     plusSlides(1);
+  }
+
+  setInterval( repeat, 3000);
