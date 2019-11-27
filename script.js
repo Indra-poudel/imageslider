936fa8ee4feaf9c3629d7362443d8b95f447ad04
indra <prince.poudel1998@gmail.com>
2019-11-27 13:16:40 +0545

    js 1 assignment added

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
+var imageWidth=400;
+
 var imageCount= document.getElementById("carousel-image-wrapper").childElementCount;
 var carouselImageWrapper=document.getElementById("carousel-image-wrapper");
-carouselImageWrapper.style.width=( (imageCount*400) + imageCount*3)+"px";
+carouselImageWrapper.style.width=( (imageCount*imageWidth))+"px";
 carouselImageWrapper.style.height=200+"px";
-for(var i=0;i<=imageCount;i++)
+
+for(var i=0;i<=imageCount-1;i++)
 {
-var span = document.createElement("span");
-   span.style.cursor= "pointer";
-    span.style.height= "15px";
-    span.style.width=" 15px";
-    span.style.margin= "0 2px";
-    span.style["background-color"]=" #bbb";
-    span.style["border-radius"]=" 50%";
-    span.style.display=" inline-block";
-    span.style.transition=" background-color 0.6s ease";
-//document.getElementById("dot-wrapper").appendChild(span);
+   var span = document.createElement("span");
+  
+   span.classList.add("dot");
+   document.getElementById("dot-wrapper").appendChild(span);
 }
+  var span= document.getElementById("dot-wrapper").children;
+  span[0].style["background-color"]="red";
 
 function plusSlides(n) {
     var styleCarouselImageWrapper= getComputedStyle(carouselImageWrapper);
@@ -22,13 +21,13 @@ function plusSlides(n) {
     var posX = posXpx.slice(0, -2);
     if(n===-1)
     {
-       if(posX<=-(imageCount-1)*404)
+       if(posX<=-(imageCount-1)*imageWidth)
        {
         carouselImageWrapper.style.left = "0px";
        
        }
        else{
-        carouselImageWrapper.style.left = (parseInt(posX)-404)+"px";
+        carouselImageWrapper.style.left = (parseInt(posX)-imageWidth)+"px";
        }
 
        
@@ -37,19 +36,33 @@ function plusSlides(n) {
     {
          if(posX>=0)
        {
-        carouselImageWrapper.style.left = -(404*(imageCount-1))+"px";
+        carouselImageWrapper.style.left = -(imageWidth*(imageCount-1))+"px";
        
        }
        else{
-        carouselImageWrapper.style.left = (parseInt(posX)+404)+"px";
+        carouselImageWrapper.style.left = (parseInt(posX)+imageWidth)+"px";
        }
-    
+   
     }
+   
   }
 
   function repeat()
   {
+     activedot();
      plusSlides(1);
+   
+     
   }
 
+function activedot()
+{
+   var styleCarouselImageWrapper= getComputedStyle(carouselImageWrapper);
+   var posXpx= styleCarouselImageWrapper.getPropertyValue('left');
+   var posX = posXpx.slice(0, -2);
+   console.log("current posx",posX);
+   var i= Math.abs(parseInt(parseInt(posX)/404)) ;
+   console.log(i);    
+}
+  
   setInterval( repeat, 3000);
